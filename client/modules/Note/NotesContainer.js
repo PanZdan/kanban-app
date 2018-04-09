cc54b418580c3363f08c945e467d09c46feb4e9f
Konrad Zdanowski <konrad-zdanowski@wp.pl>
2018-04-09 18:47:34 +0200

    Changed props in notesContainer

--- client/modules/Note/NotesContainer.js
+++ client/modules/Note/NotesContainer.js
@@ -2,23 +2,24 @@ import { connect } from 'react-redux';
 import Notes from './Notes';
 // import * as noteActions from '../Note/NoteActions';
 
-// import { deleteNote, createNote, createNotes, createNoteRequest, updateNote, deleteNoteRequest, editNote, updateNoteRequest } from "../Note/NoteActions";
-
-// const mapDispatchToProps = {
-//   editNote,
-//   updateNote: updateNoteRequest,
-//   deleteNote: deleteNoteRequest,
-//   addNote: createNoteRequest
-// };
-
-import { deleteNoteRequest, editNote, updateNoteRequest, moveWithinLane } from '../Note/NoteActions';
+import { deleteNote, createNote, createNotes, createNoteRequest, updateNote, deleteNoteRequest, editNote, updateNoteRequest, moveWithinLane } from "../Note/NoteActions";
 
 const mapDispatchToProps = {
-  onValueClick: editNote,
-  onUpdate: updateNoteRequest,
-  onDelete: deleteNoteRequest,
+  editNote,
+  updateNote: updateNoteRequest,
+  deleteNote: deleteNoteRequest,
+  addNote: createNoteRequest,
   moveWithinLane,
 };
+
+
+// const mapDispatchToProps = {
+//   onValueClick: editNote,
+//   onUpdate: updateNoteRequest,
+//   onDelete: deleteNoteRequest,
+//   addNote: createNoteRequest,
+//   moveWithinLane,
+// };
 
 export default connect(
   null,
